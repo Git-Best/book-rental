d00e1485b0135652426ef3538edf7dec18013464
chungchung <cndgks0477@naver.com>
2024-05-27 21:08:47 +0900

    feat: create book service

---
+++ service/book/src/test/java/org/msa/service/book/BookApplicationTests.java
@@ -0,0 +1,13 @@
+package org.msa.service.book;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BookApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
