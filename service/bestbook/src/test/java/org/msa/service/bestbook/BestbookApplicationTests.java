b313192f299fecea71b091d7c832f7443a9209fd
chungchung <chunghan@chungchung.local>
2025-01-06 15:28:54 +0900

    feat: create bestbook module

---
+++ service/bestbook/src/test/java/org/msa/service/bestbook/BestbookApplicationTests.java
@@ -0,0 +1,13 @@
+package org.msa.service.bestbook;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BestbookApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
